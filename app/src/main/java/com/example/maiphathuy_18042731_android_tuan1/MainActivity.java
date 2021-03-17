cf861f922e81ccac2dc15269aa8255f93f3959f6
maihuy2882000 <maihuy2882000@gmail.com>
2021-03-17 20:55:24 +0700

    first commit

---
+++ app/src/main/java/com/example/maiphathuy_18042731_android_tuan1/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.maiphathuy_18042731_android_tuan1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.bai2);
+    }
+}
